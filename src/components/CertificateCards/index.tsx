9e4ac16e25e6a29637414a187f5ddb61be6c9a07
Gokul <gokul369@outlook.com>
2026-03-04 20:50:19 +0530

    feat: remove comments

--- src/components/CertificateCards/index.tsx
+++ src/components/CertificateCards/index.tsx
@@ -12,41 +12,6 @@ function CertificateCard({
 }: achievement) {
   const Icon: IconType = iconData;
   return (
-    // <SpotlightCard
-    //   className="relative h-full w-full overflow-hidden rounded-2xl border border-white/10 bg-white/5 px-4 py-3 shadow-[0_12px_30px_rgba(0,0,0,0.55)] backdrop-blur-xl md:px-5 md:py-4"
-    //   spotlightColor={spotlight.INSPIRATIONS}
-    // >
-    //   <article className="relative flex h-full min-h-[150px] flex-col justify-between gap-3">
-    //     <div className="flex items-start justify-between gap-3">
-    //       {
-    //         <>
-    //           <Icon className="bottom-0 right-0 absolute text-green-500/25 h-20 w-20" />
-    //         </>
-    //       }
-    //       <div>
-    //         <p className="lg:text-[12px] text-[10px] font-semibold uppercase tracking-[0.25em] text-slate-400">
-    //           {type}
-    //         </p>
-    //         <h3 className="mt-1 text-sm font-medium text-slate-50 md:text-base">
-    //           {title}
-    //         </h3>
-    //         <p className="mt-1 text-xs text-slate-400">{company}</p>
-    //       </div>
-
-    //       <div className="inline-flex h-7 items-center rounded-full border border-emerald-400/30 bg-emerald-500/10 px-2.5 text-[10px] font-medium text-emerald-200">
-    //         <span className="mr-1 h-1.5 w-1.5 rounded-full bg-emerald-400" />
-    //         {year}
-    //       </div>
-    //     </div>
-
-    //     {subTitle && (
-    //       <p className="mt-1 max-w-64 text-xs leading-relaxed text-slate-300">
-    //         {subTitle}
-    //       </p>
-    //     )}
-    //   </article>
-    // </SpotlightCard>
-
     <SpotlightCard
       className="relative h-full w-full overflow-hidden rounded-2xl border border-gray-200/30 
              bg-white/60 dark:bg-black/20 px-4 py-3 shadow-lg backdrop-blur-md md:px-5 md:py-4"
